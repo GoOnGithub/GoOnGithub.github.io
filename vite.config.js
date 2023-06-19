3765d4c4f1f1f4e5b6d717cd03d9a68922212f70
Ryan Kriz <ryan.kriz@tcindustries.com>
2023-06-19 10:22:28 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/goongithub.github.io/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
@@ -20,7 +21,6 @@ export default defineConfig({
       },
     }),
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
   define: { 'process.env': {} },
   resolve: {
     alias: {
